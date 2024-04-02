d4a5f48f2ea8855900517d5707a0fdbefc1e9840
John Limber Illimani Choqueta <illimanijohn09@gmail.com>
2024-04-02 11:36:28 -0400

    version 3

---
+++ src/main/java/com/emergentes/Nuevo_20.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.emergentes;
+
+/**
+ *
+ * @author illim
+ */
+public class Nuevo_20 {
+    private String name;
+    private String last_name;
+    private String first_name;
+    private String size;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+    
+}

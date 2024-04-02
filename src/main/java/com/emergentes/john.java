343627d0c0da2209f005f8cf3e376c1f2626f928
John Limber Illimani Choqueta <illimanijohn09@gmail.com>
2024-04-02 11:12:24 -0400

    version 2

---
+++ src/main/java/com/emergentes/john.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.emergentes;
+
+/**
+ *
+ * @author illim
+ */
+public class john {
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    
+    
+    
+    
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    
+    
+           
+    
+}
